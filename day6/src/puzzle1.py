d3d661ea65d86a27d77ceba0e2e75dc5e82e8531
mascaaj_u18dt810 <alokejm@gmail.com>
2020-12-06 14:31:09 -0600

    adding new vars to pylint exception, adding notebook for puzzle 2, adding python code. Ready to merge

--- day6/src/puzzle1.py
+++ day6/src/puzzle1.py
@@ -1,6 +1,48 @@
-""" Puzzle for dayX advent of code 2020
+""" Puzzle for day6 advent of code 2020
 """
-import numpy as np
-import pandas as pd
-import math
-import re
+
+filename = '../data/data.txt'
+
+# Define functions
+def histogram(word):
+    """ Function to calculate histogram of letters in word
+    """
+    dictionary = dict()
+    for alphabet in word:
+        if alphabet not in dictionary:
+            dictionary[alphabet] = 1
+        else:
+            dictionary[alphabet] += 1
+    return dictionary
+
+with open(filename) as fn:
+    #Read in the file line by line
+    ln = fn.readline()
+    #Initialization of variables
+    [lncnt,counter,groupcounter]=[0,0,0]
+    [total,ocount]=[0,0]
+    catword=""
+    while ln:
+        #Check if not new line
+#         print("Line {}: {}".format(lncnt, ln.strip()))
+        if not ln=="\n":
+            #Count up lines, concatenate to single word, remove newline
+            counter+=1
+            ln=ln.strip("\n")
+#             print(counter)
+            catword = catword + ln
+        else:
+#             print(counter)
+#             print(catword)
+            h=histogram(catword)
+#             print(h)
+            ocount = len(h)
+#             print(ocount)
+            groupcounter+=1
+            [counter,catword]=[0,""]
+            total = total + ocount
+        ln = fn.readline()
+        lncnt+=1
+    #Print result
+    print("total groups:",groupcounter)
+    print("total:", total)
