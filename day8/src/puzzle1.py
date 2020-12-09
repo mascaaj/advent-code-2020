bed2af02e163f9ebd1fbf4f8564cd44d89890dd9
mascaaj_u18dt810 <alokejm@gmail.com>
2020-12-08 20:40:45 -0600

    Adding code comments, cleaning up formatting and commenting out test_code file

--- day8/src/puzzle1.py
+++ day8/src/puzzle1.py
@@ -4,19 +4,22 @@ import numpy as np
 import pandas as pd
 
 if __name__ == "__main__":
-
+    # Read data into dataframe
     filename = '../data/data.txt'
     colnames = ["instr","val"]
     df = pd.read_csv(filename,names=colnames,header=None,sep= " ")
     # print(df)
 
+    #Initialization
     instruction_log = []
     new_log = []
     row_check = 0
     acc_vec=[]
     acc=0
-    while row_check<=df.shape[0]:
 
+    #If the loop reaches the last row, break
+    while row_check<=df.shape[0]:
+        #Test for nop/acc/jmp, increment accumulator
         if df["instr"][row_check]=="nop":
             instruction_log.append(row_check)
             row_check+=1
@@ -33,14 +36,13 @@ if __name__ == "__main__":
             instruction_log.append(row_check)
             row_check = row_check + df["val"][row_check]
     #         print("jmp")
-
+        #Test to see if unique values in the array of rows travelled == array
+        #of rows travelled. If not, loop has started recursion
         acc_vec.append(acc)
         new_log = np.unique(instruction_log)
         new_log = new_log.tolist()
-    #     print(row_check)
-    #     print(instruction_log)
-    #     print(new_log)
-    #     print(acc_vec)
+
+        #If recursion, break, print value to console
         if len(instruction_log)!=len(new_log):
             print("Found recursion")
             print("accumulator value:",acc_vec[-2])
--- day8/src/puzzle2.py
+++ day8/src/puzzle2.py
@@ -4,19 +4,19 @@ import numpy as np
 import pandas as pd
 
 if __name__ == "__main__":
-
+    # Read data into dataframe
     filename = '../data/data.txt'
     colnames = ["instr","val","jmp","nop"]
     df = pd.read_csv(filename,names=colnames,header=None,sep= " ")
     # print(df)
 
-    # instruction_log = []
-    # new_log = []
+    #Initialization
     row_check = 0
     acc_vec=[]
     jmp_vec = []
     acc=0
 
+    #If the loop reaches the last row, break
     while row_check<df.shape[0]:
         print("accumulator_value:",acc)
         print("flag")
@@ -24,7 +24,9 @@ if __name__ == "__main__":
         acc=0
         instruction_log = []
         new_log = []
+        #flag in recursive loop, restarts the filter (might have redundant code)
         while flag == 0:
+            #Test for nop/acc/jmp, increment accumulator
             if df["instr"][row_check]=="nop":
                 instruction_log.append(row_check)
                 row_check+=1
@@ -42,31 +44,34 @@ if __name__ == "__main__":
                 if df["val"][row_check]<0:
                     print("negative jump")
                 row_check = row_check + df["val"][row_check]
-
+            #Test to see if unique values in the array of rows travelled == array
+            #of rows travelled. If not, loop has started recursion
             acc_vec.append(acc)
             new_log = np.unique(instruction_log)
             new_log = new_log.tolist()
+            #If recursion, break, capture the vector of jmp in the array found
             if len(instruction_log)!=len(new_log):
                 flag=1
-                print("here")
+                print("recursion found")
                 jmp_vec = []
                 for row in range(len(new_log),0,-1):
                     index = instruction_log[row]
                     if df["instr"][index]=="jmp":
                         jmp_vec.append(index)
-                print(len(jmp_vec))
+                # print(len(jmp_vec))
                 break
         print(acc)
         break
 
     print(jmp_vec)
 
-    print(jmp_vec)
-
-    for i in range(0, len(jmp_vec)):
+    # Read in dataframe on each loop, change one jmp value, test to reach end of file
+    # Prolly not efficient, but should break when no more traversal thru dataframe is possible
+    for i in enumerate(jmp_vec):
         colnames = ["instr","val","jmp","nop"]
         df_temp = pd.read_csv(filename,names=colnames,header=None,sep= " ")
-        df_temp["instr"][jmp_vec[i]]="nop"
+
+        df_temp["instr"][i[1]]="nop"
         print("accumulator_value:",acc)
         flag=0
         acc=0
@@ -93,5 +98,5 @@ if __name__ == "__main__":
 
             if len(instruction_log)!=len(new_log):
                 flag=1
-                print("here")
+                print("Recursion, Restarting with new value")
                 break
