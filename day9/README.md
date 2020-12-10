bd795f358d54537e42c008be9c2fff724a90d183
mascaaj_u18dt810 <alokejm@gmail.com>
2020-12-09 20:33:49 -0600

    adding solution and updating linter rc file

--- day9/README.md
+++ day9/README.md
@@ -5,6 +5,7 @@ Advent of Code : day9
 
 Puzzles
 ------------
-Puzzle 1 statement here <- Solved
+The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers before it. What is the first number that does not have this property? <- Solved
 
-Puzzle 2 statement here <- Unsolved
+To find the encryption weakness, add together the smallest and largest number in this contiguous range.
+What is the encryption weakness in your XMAS-encrypted list of numbers? <- Solved
--- day9/src/puzzle2.py
+++ day9/src/puzzle2.py
@@ -1,9 +1,51 @@
 """ Puzzle for dayX advent of code 2020
 """
+from itertools import combinations
 import numpy as np
-import pandas as pd
-import math
-import re
-
 
 if __name__ == "__main__":
+    #Read file into numpy array
+    filename = "../data/data.txt"
+    data = np.genfromtxt(filename, delimiter=',')
+
+    #intialization
+    preamble_count=25
+    result_array=[]
+    flag=1
+    #Prefill the arrary with zero using preamble length
+    for arr in range(0,preamble_count):
+        result_array.append(0)
+
+    for i in enumerate(data):
+        #Report data value if flag is zero
+        if flag==0:
+            flagged_num = i[0]-1+preamble_count
+        result_array.append(flag)
+        #Shift the preamble array
+        preamble = data[i[0]:i[0]+preamble_count]
+        #Test for bounds
+        if (i[0]+preamble_count)<len(data):
+            #Assign the test value
+            test_val = data[i[0]+preamble_count]
+            #This is the magic right here to find the combinations
+            a = combinations(preamble,2)
+            flag=0
+            #iterate over combinations and sum
+            for j in enumerate(list(a)):
+                eval_sum = sum(j[1])
+                if test_val == eval_sum:
+                    flag=1
+        else:
+            break
+    #Return output
+    flagged_value = data[flagged_num]
+    print("index : {}, value: {}".format(flagged_num,flagged_value))
+
+    for i in range(0,len(data)):
+        for j in range(i,len(data)):
+            data_sum = sum(data[i:j])
+            if data_sum == flagged_value:
+                print("max value :", max(data[i:j]))
+                print("min value :", min(data[i:j]))
+                print("min max sum :",max(data[i:j]) + min(data[i:j]))
+                break
