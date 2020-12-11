14442c0879b8124748bb52e6d0484c1c137ac1a9
mascaaj_u18dt810 <alokejm@gmail.com>
2020-12-11 16:07:02 -0600

    adding puzzle 1 solution

--- day10/README.md
+++ day10/README.md
@@ -5,6 +5,7 @@ Advent of Code : day10
 
 Puzzles
 ------------
-Puzzle 1 statement here <- Solved
+Find a chain that uses all of your adapters to connect the charging outlet to your device's built-in adapter and count the joltage differences between the charging outlet, the adapters, and your device. What is the number of 1-jolt differences multiplied by the number of 3-jolt differences? <- Solved
 
-Puzzle 2 statement here <- Unsolved
+What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device? <- Unsolved
+Needs dynamic programming, will return to this on weekend
--- day10/src/puzzle1.py
+++ day10/src/puzzle1.py
@@ -1,9 +1,43 @@
-""" Puzzle for dayX advent of code 2020
+""" Puzzle for day10 advent of code 2020
 """
 import numpy as np
-import pandas as pd
-import math
-import re
-
 
 if __name__ == "__main__":
+    # Readin file
+    filename = "../data/data.txt"
+    data = np.genfromtxt(filename, delimiter=',')
+    print(data)
+    #Initialization
+    result_array=[0]
+    diff_array = []
+    last_val=0
+    # Sort the data to improve the search
+    data = np.sort(data)
+    # Iterate and check add sum and results array
+    while len(result_array) < len(data):
+        print(len(result_array))
+        for i in enumerate(data):
+            diff = i[1] - last_val
+            if diff == 1:
+                last_val = i[1]
+                result_array.append(last_val)
+                diff_array.append(diff)
+            else:
+                if diff == 2:
+                    last_val = i[1]
+                    result_array.append(last_val)
+                    diff_array.append(diff)
+                else:
+                    if diff == 3:
+                        last_val = i[1]
+                        result_array.append(last_val)
+                        diff_array.append(diff)
+
+    #Process output to get desired output.
+    print(result_array)
+    print(diff_array)
+    b,c = np.unique(diff_array,return_counts=True)
+    print("1 counts:", c[0])
+    print("3 counts:", c[1]+1)
+    product = c[0] * (c[1]+1)
+    print(product)
